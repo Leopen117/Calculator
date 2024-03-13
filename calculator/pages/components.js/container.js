1f740ed5ca8c6737b7d686549ca0dd8392c7bed2
Leon Schwedler <leon@EO-LA-021>
2024-03-13 13:24:22 +0100

    add the switch-case and finish the project

--- calculator/pages/components.js/container.js
+++ calculator/pages/components.js/container.js
@@ -8,138 +8,168 @@ const Container = () => {
   const [cache, setCache] = useState("");
   const [operator, setOperator] = useState("");
 
+  const addToDisplay = (btnVal) => {
+    switch (btnVal) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case ".": {
+        setDisplayValue(displayValue + btnVal);
+        break;
+      }
+      case "AC": {
+        setDisplayValue(""), setCache(""), setOperator("");
+        break;
+      }
+      case "+/-": {
+        setDisplayValue(displayValue * -1);
+        break;
+      }
+      case "+":
+      case "-":
+      case "x":
+      case "/": {
+        setOperator(btnVal);
+        setCache(displayValue);
+        setDisplayValue("");
+        break;
+      }
+      case "=": {
+        switch (operator) {
+          case "+": {
+            setDisplayValue(parseFloat(cache) + parseFloat(displayValue));
+            setCache(displayValue);
+            break;
+          }
+          case "-": {
+            setDisplayValue(parseFloat(cache) - parseFloat(displayValue));
+            setCache(displayValue);
+            break;
+          }
+          case "x": {
+            setDisplayValue(parseFloat(cache) * parseFloat(displayValue));
+            setCache(displayValue);
+            break;
+          }
+          case "/": {
+            setDisplayValue(parseFloat(cache) / parseFloat(displayValue));
+            setCache(displayValue);
+            break;
+          }
+        }
+      }
+    }
+  };
+
   return (
-    <div className="container bg-dark w-50 h-75 ">
+    <div className=" rounded-4 container bg-dark">
       <div className="row">
         <Btn className="col rounded-top btn-dark" value="Calculator"></Btn>
       </div>
       <Display className="row col " displayValue={displayValue}></Display>
       <div className="row">
         <Btn
-          setCache={setCache}
-          setDisplayValue={setDisplayValue}
-          setOperator={setOperator}
+          onClick={() => addToDisplay("AC")}
           className="col-md-6 btn-primary"
           value="AC"
         ></Btn>
         <Btn
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
+          onClick={() => addToDisplay("+/-")}
           className="col btn-primary"
           value="+/-"
         ></Btn>
         <Btn
-          setOperator={setOperator}
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
-          setCache={setCache}
+          onClick={() => addToDisplay("/")}
           className="col btn-warning"
           value="/"
         ></Btn>
       </div>
       <div className="row">
         <Btn
+          onClick={() => addToDisplay("7")}
           className="col btn-dark"
           value="7"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
+          onClick={() => addToDisplay("8")}
           className="col btn-dark"
           value="8"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
+          onClick={() => addToDisplay("9")}
           className="col btn-dark"
           value="9"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
-          setOperator={setOperator}
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
-          setCache={setCache}
+          onClick={() => addToDisplay("x")}
           className="col btn-warning"
           value="x"
         ></Btn>
       </div>
       <div className="row">
         <Btn
+          onClick={() => addToDisplay("4")}
           className="col btn-dark"
           value="4"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
+          onClick={() => addToDisplay("5")}
           className="col btn-dark"
           value="5"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
+          onClick={() => addToDisplay("6")}
           className="col btn-dark"
           value="6"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
-          setOperator={setOperator}
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
-          setCache={setCache}
+          onClick={() => addToDisplay("-")}
           className="col btn-warning"
           value="-"
         ></Btn>
       </div>
       <div className="row">
         <Btn
+          onClick={() => addToDisplay("1")}
           className="col btn-dark"
           value="1"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
+          onClick={() => addToDisplay("2")}
           className="col btn-dark"
           value="2"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
+          onClick={() => addToDisplay("3")}
           className="col btn-dark"
           value="3"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
-          setOperator={setOperator}
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
-          setCache={setCache}
+          onClick={() => addToDisplay("+")}
           className="col btn-warning"
           value="+"
         ></Btn>
       </div>
-      <div className="row">
+      <div className="row rounded-bottom">
         <Btn
+          onClick={() => addToDisplay("0")}
           className="col-md-6 btn-dark"
           value="0"
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
+          onClick={() => addToDisplay(".")}
           className="col btn-dark"
           value="."
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
         ></Btn>
         <Btn
-          operator={operator}
-          cache={cache}
-          setCache={setCache}
-          displayValue={displayValue}
-          setDisplayValue={setDisplayValue}
+          onClick={() => addToDisplay("=")}
           className="col btn-warning"
           value="="
         ></Btn>
--- calculator/pages/index.js
+++ calculator/pages/index.js
@@ -4,7 +4,9 @@ import { Container } from "./components.js/container";
 export default function Home() {
   return (
     <div className=" bg-secondary text-center vh-100">
-      <Container></Container>
+      <div className=" h-50 position-absolute top-50 start-50 translate-middle">
+        <Container></Container>
+      </div>
     </div>
   );
 }
